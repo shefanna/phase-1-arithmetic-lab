249f5bc47041223a27e51e83a1213c8f960c8ac0
shefinannathampy <shefin04annathampy94@gmail.com>
2023-03-19 20:09:31 -0400

    arithimatic lab

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 let num1 = 31;
 let num2 = 2;
-
-let multiply = num1 * num2; // multiply is set to 62
-
-let random = Math.floor(Math.random() * 100) + 1; // random is set to a random integer between 1 and 100
-
-let num3 = 15;
-let num4 = 11;
-
-let mod = num3 % num4; // mod is set to 4
-
-let numbers = [5, 10, 15, 20, 8];
-let max = Math.max(...numbers); // max is set to 20
+let multiply = num1 * num2; 
+let random = Math.floor(Math.random() * 100) + 1; 
+let num3 = 24;
+let num4 = 20;
+let mod = num3 % num4; 
+let numbers = [19, 11, 2, 20, 3,9,4,15];
+let max = Math.max(...numbers); 
